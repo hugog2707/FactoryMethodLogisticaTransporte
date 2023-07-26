b0e4d0e3d184abc4f6649aab88862775ceafff2b
hugo garcia <hgarcia@audisoft.com>
2023-07-26 13:48:21 -0500

    feature: changed in the names to the columns and added JsonIgnore avoid circular reference

--- src/main/java/com/demo/FactoryMethodLogisticaTransporte/models/Inventario.java
+++ src/main/java/com/demo/FactoryMethodLogisticaTransporte/models/Inventario.java
@@ -1,5 +1,6 @@
 package com.demo.FactoryMethodLogisticaTransporte.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -13,13 +14,24 @@ public class Inventario {
     public int idInventario;
 
     @ManyToOne
-    @JoinColumn(name = "tipo_inventarioId")
+    @JoinColumn(name = "tipo_inventario_id")
     public TipoInventario tipoInventario;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "inventario")
     public List<Productos> productos;
 
     @OneToOne
     @JoinColumn(name = "id_personal")
     public Personal encargado;
+
+    public void agregarProducto(Productos producto){
+        this.productos.add(producto);
+        producto.setInventario(this);
+    }
+
+    public void eliminarProducto(Productos producto){
+        this.productos.remove(producto);
+        producto.setInventario(null);
+    }
 }
