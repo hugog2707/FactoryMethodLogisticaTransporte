9db90ad3a5b1a6506e7be2dafa132b74b10e7088
hugo garcia <hgarcia@audisoft.com>
2023-07-26 13:45:12 -0500

    separate controllers

--- src/main/java/com/demo/FactoryMethodLogisticaTransporte/controller/LogisticaTierraController.java
+++ src/main/java/com/demo/FactoryMethodLogisticaTransporte/controller/LogisticaTierraController.java
@@ -1,25 +1,9 @@
 package com.demo.FactoryMethodLogisticaTransporte.controller;
 
-import com.demo.FactoryMethodLogisticaTransporte.logistica.LogisticaTierra;
-import com.demo.FactoryMethodLogisticaTransporte.logistica.RutaEntrega;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/logisticaTierra")
-@RestController
+@Controller
 public class LogisticaTierraController {
-    LogisticaTierra logisticaTierra = new LogisticaTierra();
-    public String origen;
-    public String destino;
-
-    @PostMapping("/distanciaTiempo")
-    //recibe un json con los datos de la ruta de entrega
-    public String distanciaTiempo(@RequestBody RutaEntrega rutaEntrega){
-        origen = rutaEntrega.getOrigen();
-        destino = rutaEntrega.getDestino();
-
-        logisticaTierra.planEntrega(origen, destino);
-
-        return logisticaTierra.distanciaToString() + " " + logisticaTierra.tiempoToString();
-    }
 }
